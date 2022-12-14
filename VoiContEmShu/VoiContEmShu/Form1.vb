a75e69b6b7270b190629b7689c17e3d52f5462a3
99ass1embler <120421994+thomasisttoll@users.noreply.github.com>
2022-12-14 14:40:54 +0100

    Add files via upload
    Voice Controlled Emergency Shutdowner - or personal assistent.
    If you get removed from your PC and you want to shut it down just by saying it.
    Original Project from YTer Pillazo - Visual Basic Voice Recognition

---
+++ VoiContEmShu/VoiContEmShu/Form1.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Speech
+Imports System.Threading
+
+Public Class Form1
+
+    Dim WithEvents recognition As New Recognition.SpeechRecognitionEngine
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        recognition.SetInputToDefaultAudioDevice()
+
+        Dim grammar As New Recognition.SrgsGrammar.SrgsDocument
+
+        Dim grammarRule As New Recognition.SrgsGrammar.SrgsRule("Anti_Raid_Emergency_Shutdowner")
+
+        Dim commands As New Recognition.SrgsGrammar.SrgsOneOf("Computer ausschalten", "Programm beenden", "Bitcoin")
+
+        grammarRule.Add(commands)
+
+        grammar.Rules.Add(grammarRule)
+
+        grammar.Root = grammarRule
+
+        recognition.LoadGrammar(New Recognition.Grammar(grammar))
+
+        recognition.RecognizeAsync()
+
+    End Sub
+
+
+    Private Sub reco_RecognizeCompleted(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognizeCompletedEventArgs) Handles recognition.RecognizeCompleted
+
+        recognition.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_SpeechRecognized(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognitionEventArgs) Handles recognition.SpeechRecognized
+
+        Select Case e.Result.Text
+
+            Case "Computer ausschalten"
+
+                Dim synth As New Synthesis.SpeechSynthesizer
+                synth.SpeakAsync("Computer wird ausgeschalten.")
+                Process.Start("shutdown", "-s -t 5")
+
+
+            Case "Programm beenden"
+
+                Dim synth As New Synthesis.SpeechSynthesizer
+                synth.SpeakAsync("Bis zum nächsten Mal Habibi")
+                Thread.Sleep("4000")
+                Environment.FailFast("Holzhammermethode weil RCW-Cleanup eh niemanden kümmert")
+
+
+                '    'Simply add New cases here and the associated command in form1_load
+                'Case "Bitcoin"
+
+                '    Dim synth As New Synthesis.SpeechSynthesizer
+                '    synth.SpeakAsync("Alle Kryptos werden an 99ass1embler gesendet. Trololololo")
+
+
+
+        End Select
+
+    End Sub
+
+End Class
