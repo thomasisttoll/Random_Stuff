500380b98df908fa826d921307e6b9e288eb6a89
99ass1embler <120421994+thomasisttoll@users.noreply.github.com>
2023-05-02 20:48:56 +0200

    Add files via upload
    Vox - a simple voice controlled Assistant. Usefull to shutdown your PC via voice command (if you get removed from the PC or if you are simply lazy). Effective in combination with VeraCrypt^^
    Waifu-KI-Voice will be integrated someday...

---
+++ Vox/Vox/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vox
+{
+    internal class Program
+    {
+        static SpeechRecognitionEngine recognition = new SpeechRecognitionEngine();
+
+        static void Main(string[] args)
+        {
+            recognition.SetInputToDefaultAudioDevice();
+
+            SrgsDocument grammar = new SrgsDocument();
+            SrgsRule grammarRule = new SrgsRule("Anti_Raid_Emergency_Shutdowner");
+            SrgsOneOf commands = new SrgsOneOf("Computer ausschalten", "Programm beenden", "Bitcoin");
+
+            grammarRule.Add(commands);
+            grammar.Rules.Add(grammarRule);
+            grammar.Root = grammarRule;
+
+            recognition.LoadGrammar(new Grammar(grammar));
+            recognition.RecognizeAsync(RecognizeMode.Multiple);
+
+            recognition.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(SpeechRecognizedHandler);
+            recognition.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(RecognizeCompletedHandler);
+
+            Console.WriteLine("Sprechen Sie einen der folgenden Befehle:");
+            Console.WriteLine("- Computer ausschalten");
+            Console.WriteLine("- Programm beenden");
+            Console.WriteLine("- Bitcoin");
+
+            Console.ReadLine();
+        }
+
+        static void RecognizeCompletedHandler(object sender, RecognizeCompletedEventArgs e)
+        {
+            recognition.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        static void SpeechRecognizedHandler(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "Computer ausschalten":
+                    SpeechSynthesizer synth1 = new SpeechSynthesizer();
+                    synth1.SpeakAsync("Computer wird ausgeschaltet.");
+                    Process.Start("shutdown", "-s -t 50");
+                    break;
+
+                case "Programm beenden":
+                    SpeechSynthesizer synth2 = new SpeechSynthesizer();
+                    synth2.SpeakAsync("Bis zum nächsten Mal Habibi");
+                    Thread.Sleep(4000);
+                    Environment.FailFast("Holzhammermethode weil RCW-Cleanup eh niemanden kümmert");
+                    break;
+
+                case "Bitcoin":
+                    SpeechSynthesizer synth3 = new SpeechSynthesizer();
+                    synth3.SpeakAsync("Alle Kryptos werden an 99ass1embler gesendet. Trololololo");
+                    break;
+            }
+        }
+    }
+}
